6d227873a5d0e5ff559a2294fcc7d02fe103de20
Anjie Scheuren <emailanjie@gmail.com>
2015-12-09 14:46:03 -0600

    updated w02d02 HW

--- w02/d02/starter/js/main.js
+++ w02/d02/starter/js/main.js
@@ -4,33 +4,47 @@
 
 //How old are you? If age < 13 - "You know nothing, Jon Snow. You're too young to play." If age >= 13 - "Good luck. You'll need it."
 var age = prompt("How old are you?");
-var face;
 age = parseInt(age);
 if (age < 13) {
-  console.log("You know nothing, Jon Snow. You're too young to play.");
-}
-else if (age >= 13) {
-  console.log("Good luck. You'll need it.");
-}
-else {
-  console.log("Try again");
+    console.log("You know nothing, Jon Snow. You're too young to play.");
+} else if (age >= 13) {
+    console.log("Good luck. You'll need it.");
+} else {
+    console.log("Try again");
 }
 
 var settle = prompt("You have travelled from a faraway planet and have decided to fight in the Game of Thrones. When you play the Game of Thrones, there is no middle-ground. You either win or you die. In order to be competitive, you need to find a place to settle. Where would you like to settle - Braavos, King's Landing, or Dorne?")
 
-if (settle === "Braavos") {
-  prompt("Do you have a face? yes or no");
-  if (face === "yes" || face === "Yes") {
-  console.log("Winter is coming. You're dead.");
-}
-else if (face === "no" || face === "No") {
-  console.log("The throne is yours.");
-}
-else {
-  console.log("Try again");
+if (settle === "Braavos" || settle === "braavos") {
+    var face = prompt("Do you have a face? Yes or No");
+    if (face === "yes" || face === "Yes") {
+        console.log("Winter is coming. You're dead.");
+    } else if (face === "no" || face === "No") {
+        console.log("The throne is yours.");
+    } else {
+        console.log("Try again");
+    }
+} else if (settle === "King's Landing" || settle === "Kings Landing" || settle === "king's landing" || settle === "kings landing") {
+    var hair = prompt("What color is your hair? Blonde, Black, or Red");
+    if (hair === "Blonde" || hair === "blonde") {
+        console.log("Winter is coming. You're dead.");
+    } else if (hair === "Black" || hair === "black" || hair === "Red" || hair === "red") {
+        console.log("The throne is yours.");
+    } else {
+        console.log("Try again.");
+    }
+} else if (settle === "Dorne" || settle === "dorne") {
+    var gender = prompt("What is your gender?");
+    if (gender === "Male" || gender === "male") {
+        console.log("Winter is coming. You're dead.");
+    } else if (gender === "Female" || gender === "female") {
+        console.log("The throne is yours.");
+    } else {
+        console.log("Try again.")
+    }
 }
 
-}
+
 //"In order to be competitive, you need to find a place to settle.)
 //"You have travelled from a faraway planet and have decided to fight in the Game of Thrones.
 //When you play the Game of Thrones, there is no middle-ground. You either win or you die."
