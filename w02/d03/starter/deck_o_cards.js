4d28d9b389578338c7514be0bac65b57fc39e05b
Anjie Scheuren <emailanjie@gmail.com>
2015-12-09 22:57:25 -0600

    w02d03 HW

--- w02/d03/starter/deck_o_cards.js
+++ w02/d03/starter/deck_o_cards.js
@@ -1,21 +1,43 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays. 
+// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
 	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  	var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  // Create the arrays for the deck, and shuffled deck
+  var cards = [];
+  var shuffledCards = [];
+  var poker = [];
 
-	// Create the arrays for the deck, and shuffled deck
-	var cards = [];
-	// Make 52 card objects and store them in the "cards" array
+
+  // Make 52 card objects and store them in the "cards" array
+  for (var i = 0; i < suits.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      var card = {
+        suit: suits[i],
+        value: values[j]
+      };
+      cards.push(card);
+    }
+  }
 
 	// Shuffle the Deck
+  shuffledCards = shuffle(cards);
 
 	// Pull the top card
+  var topCard = shuffledCards[0];
 
 	// Console log the results
-}
+  console.log("The deck has " + cards.length + " cards. The top card is the " + topCard.value + " of " + topCard.suit + ".");
+  console.log("Your poker hand is: ");
+  //Make a new array that pulls the top 5 cards and gives you a hand of poker
+  for (var i = 0; i <= 4; i++) {
+    var pokerHand = shuffledCards[i];
+    poker.push(pokerHand);
+    console.log(poker[i].value + " of " + poker[i].suit);
+    }
+  };
 
-// Fisher-Yates Shuffle 
+// Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
 function shuffle(array) {
     var counter = array.length, temp, index;
@@ -35,4 +57,6 @@ function shuffle(array) {
     }
 
     return array;
+
+
 }
