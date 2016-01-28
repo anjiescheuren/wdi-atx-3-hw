94fc3c588ca2ef849ab0294c26aed68cc1eadf15
Anjie Scheuren <emailanjie@gmail.com>
2016-01-28 17:26:49 -0600

    update w07d03 homework

--- w07/d03/starter/routes/index.js
+++ w07/d03/starter/routes/index.js
@@ -1,38 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var compliments = [
-"Your instructors love you",
-"High five!",
-"Is it Ruby Tuesday yet?",
-"It's almost beer o'clock",
-"The Force is strong with you",
-"You're simply marvelous",
-"You're the bees knees",
-"You're the coolest"
+    "Your instructors love you",
+    "High five!",
+    "Is it Ruby Tuesday yet?",
+    "It's almost beer o'clock",
+    "The Force is strong with you",
+    "You're simply marvelous",
+    "You're the bees knees",
+    "You're the coolest"
 ];
 
-var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
+var colors = ["#FFBF00", "#0080FF", "#01DF3A", "#FF0080"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var i = Math.floor(Math.random() * compliments.length);
-  var newCompliment = compliments[i];
+    var i = Math.floor(Math.random() * compliments.length);
+    var newCompliment = compliments[i];
 
-  var j = Math.floor(Math.random()*colors.length);
-  var newColor = colors[j];
+    var j = Math.floor(Math.random() * colors.length);
+    var newColor = colors[j];
 
-  res.render('index', { title: 'Hey there', compliment: newCompliment, backgroundColor: newColor});
+    res.render('index', {
+        title: 'Hey there',
+        compliment: newCompliment,
+        backgroundColor: newColor
+    });
 });
 
-router.get('/:name', function(req, res, next){
-  var name = req.params.name;
-  var i = Math.floor(Math.random() * compliments.length);
-  var newCompliment = compliments[i];
+router.get('/:name', function(req, res, next) {
+    var name = req.params.name;
+    var i = Math.floor(Math.random() * compliments.length);
+    var newCompliment = compliments[i];
 
-  var j = Math.floor(Math.random()*colors.length);
-  var newColor = colors[j];
+    var j = Math.floor(Math.random() * colors.length);
+    var newColor = colors[j];
 
-  res.render('index', { title: 'Hey there, ' + name, compliment: newCompliment, backgroundColor: newColor});
+    res.render('index', {
+        title: 'Hey there, ' + name,
+        compliment: newCompliment,
+        backgroundColor: newColor
+    });
 });
 
 // var compliments = ["Your instructors love you", "High five = ^5", "Is it Ruby Tuesday yet?", "It's almost beer o'clock", "The Force is strong with you"];
