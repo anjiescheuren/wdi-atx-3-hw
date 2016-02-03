82a21bf59af092305d19e41d2db540cd34d1125c
Anjie Scheuren <emailanjie@gmail.com>
2016-02-02 21:22:00 -0600

    not sure. some changes to class files

--- w07/d04/starter/mongoose_compliment/routes/index.js
+++ w07/d04/starter/mongoose_compliment/routes/index.js
@@ -3,33 +3,51 @@ var router = express.Router();
 var Compliment = require('../models/compliment');
 
 function randomColor() {
-	var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
-	return colors[Math.floor(Math.random()*colors.length)];
+    var colors = ["#FFBF00", "#0080FF", "#01DF3A", "#FF0080"];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function randomCompliment(compArray) {
+    var compliments = compArray;
+    return compliments;
 }
 
 /* GET compliment form. */
 router.get('/compliment_form', function(req, res, next) {
-	var color = randomColor();
-	res.render('compliment_form', { title: 'WDI Emergency Compliment', color: color });
+    var color = randomColor();
+    res.render('compliment_form', {
+        title: 'WDI Emergency Compliment',
+        color: color
+    });
 });
 
 /* GET home page with queried name. */
 router.get('/(:name)?', function(req, res, next) {
-	name = req.params.name || "Friend";
-	var color = randomColor();
+    name = req.params.name || "Friend";
+    var color = randomColor();
 
-	// USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW IN THE DATABASE CALLBACK
-
-	var compliment = null; // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
-	res.render('index', { title: 'WDI Emergency Compliment', color: color, name: name, compliment: compliment });
+    // USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW IN THE DATABASE CALLBACK
+    router.get('/', function(req, res, next) {
+        res.render('compliment_form', {
+            title: 'WDI Emergency Compliment',
+            color: color
+        });
+    });
+    var text = randomCompliment(data); // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
+    res.render('index', {
+        title: 'WDI Emergency Compliment',
+        color: color,
+        name: name,
+        compliment: text.compliment
+    });
 });
 
 /* POST compliment. */
 router.post('/', function(req, res, next) {
-	var newCompliment = req.body.compliment;
+    var newCompliment = req.body.compliment;
 
-	// USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
-	res.redirect('/');
+    // USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
+    res.redirect('/');
 });
 
 module.exports = router;
