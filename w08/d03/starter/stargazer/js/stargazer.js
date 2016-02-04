2f9a56e7a60d6129f62b22b8afe6fccefcbfe982
Anjie Scheuren <emailanjie@gmail.com>
2016-02-04 17:33:06 -0600

    stargazer homework

--- w08/d03/starter/stargazer/js/stargazer.js
+++ w08/d03/starter/stargazer/js/stargazer.js
@@ -1,5 +1,7 @@
 function search(name) {
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
   // requires "jsonp" datatype.
-  
+  var searchTerm = $('#lookUP').val();
+  console.log(searchTerm);
+
 }
